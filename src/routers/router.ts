d7d2424fef8a1841866505bbf00758afe754e581
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-13 18:07:47 -0300

    feat: rota post /users para criar novo usuario

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import productsController from '../controllers/products.controller';
+import usersController from '../controllers/users.controller';
 
 const routers = Router();
 
 routers.post('/products', productsController.createProduct);
 
 routers.get('/products', productsController.getAll);
+
+routers.post('/users', usersController.createUser);
 
 export default routers;
