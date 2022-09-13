182a504ce2505b23f63bc35c367d4da54bd3009a
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-13 16:26:36 -0300

    feat: função na camada model para listar todos os produtos cadastrados

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,6 +1,6 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import { ICreateProducts } from '../interfaces';
+import { ICreateProducts, IProduct } from '../interfaces';
 
 const createProduct = async (product: ICreateProducts): Promise<ResultSetHeader> => {
   const query = 'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)';
@@ -8,4 +8,10 @@ const createProduct = async (product: ICreateProducts): Promise<ResultSetHeader>
   return result;
 };
 
-export default { createProduct };
+const getAll = async (): Promise<IProduct[]> => {
+  const query = 'SELECT * FROM Trybesmith.Products';
+  const [result] = await connection.execute(query);
+  return result as IProduct[];
+};
+
+export default { createProduct, getAll };
