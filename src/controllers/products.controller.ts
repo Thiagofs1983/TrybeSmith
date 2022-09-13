874e4ec686a507397f04362acaf3989f3eb175c6
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-13 16:36:23 -0300

    feat: função na camada controller para retornar todos os produtos

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -2,10 +2,15 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import productServices from '../services/product.services';
 
-const createProduct = async (req: Request, res: Response) => {
+const createProduct = async (req: Request, res: Response): Promise<void> => {
   const newProduct = req.body;
   const result = await productServices.createProduct(newProduct);
   res.status(StatusCodes.CREATED).json(result);
 };
 
-export default { createProduct };
+const getAll = async (req: Request, res: Response): Promise<void> => {
+  const result = await productServices.getAll();
+  res.status(StatusCodes.OK).json(result);
+};
+
+export default { createProduct, getAll };
