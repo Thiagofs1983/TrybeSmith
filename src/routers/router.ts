1f9c6ae39c05f314def5f7fd1e84ffcfde77fca8
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-13 16:14:31 -0300

    feat: rota post para criar um novo produto

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import productsController from '../controllers/products.controller';
 
 const routers = Router();
+
+routers.post('/products', productsController.createProduct);
 
 export default routers;
