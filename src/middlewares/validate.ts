3bf8931417dece04bba090603ac2199dba984e80
Thiago Fonseca de Souza <t.fs.2007@hotmail.com>
2022-09-14 07:58:11 -0300

    fix: chama a função required no joi do password

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -8,7 +8,7 @@ const validateLogin = (req: Request, res: Response, next: NextFunction) => {
   const user: ILogin = req.body;
   const schema = Joi.object({
     username: Joi.string().min(2).required(),
-    password: Joi.string().min(2).required,
+    password: Joi.string().min(2).required(),
   });
   const { error } = schema.validate(user);
   if (error) {
